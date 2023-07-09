a14eb8f0c9afb253a0305a77477423d0ad1e3f60
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-09 11:18:56 +0200

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
-done
+#!/usr/bin/python3
+# Author - Okeke Makuochukwu
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")

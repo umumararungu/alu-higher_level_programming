13b6abd2615b7648af2563124a02efb1eca29e70
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-09 11:54:26 +0200

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,6 @@
-4-hidden_discovery.py
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)

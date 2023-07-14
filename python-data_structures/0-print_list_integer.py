b5f1eb1de14a9bd52e0a4827752d9e9d7ba1f10a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-14 20:33:34 +0200

    Update 0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def first_list(my_list=[1, 2, 3]):
-    for num in my_list:
-        print(".format(", "{:d}", num)
+def first_list(my_list=[]):
+    for num in range(len(my_list):
+        print("{:d}".format(my_list[num])

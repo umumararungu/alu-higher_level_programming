0851d80e958509b088e42aee9320fc57a717dc00
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 22:55:32 +0200

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        index = my_list[idx]
+        index = ((my_list[idx]) - 1)
         remove = my_list.remove(index)
         return my_list

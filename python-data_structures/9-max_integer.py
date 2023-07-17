e4bfffa51ab4008698fea4f2625f6158a8a79b24
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 21:35:37 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,8 +4,7 @@ def max_integer(my_list=[]):
     if my_list == a:
         return None
     else:
-        if print(all([isinstance(item, int) for item in my_list])):
-            large_number = None
+         large_number = None
             for number in my_list:
                 if large_number is None or large_number < number:
                     print(number)

1c29c096d05dc0015c996df4b83cfccbea7f603a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 19:06:20 +0200

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-  
   if idx < 0 and idx > len(my_list):
     return my_list
-  my_list[idx] = element
-  return my_list
+  copy_list = [y for y in my_list]
+  copy_list[idx] = element
+  return copy_list

77da30f123d24d36c7b074ae15ad790b379a113b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 20:18:21 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sentence = "Holberton is so cool"
-    length, first = multiple_returns(sentence)
-    print("Length: {:d} - First character: {}".format(length, first))
+    length = len(sentence)
+    first = sentence[0]
+    if length > 0 else None
+    return length, first

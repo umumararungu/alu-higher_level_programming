667c93ba4fbbb76de92fb632d060a2d762b619c6
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 22:37:06 +0200

    Update 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-   result = []
-   for number in my_list:
-      result.append(number % 2 == 00)
-   return result
+    result = []
+    for number in my_list:
+       result.append(number % 2 == 00)
+    return result

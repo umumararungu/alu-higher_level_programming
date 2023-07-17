c7ef7860811b081689276f528fb83712a8501fb7
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 20:02:56 +0200

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,6 @@
-7-add_tuple.py
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = turple_a + (0, 0)
+    b = turple_b + (0, 0)
+    sum _turple= (a[0] + b[0], a[1] + b[1])
+    return sum_turple

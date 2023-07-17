5c9e69185810adb73738b24f4c6d95096482b15b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 18:40:52 +0200

    Update 3-print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,5 @@
-3-print_reversed_list_integer.py
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))

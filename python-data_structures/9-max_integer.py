9bae2c661b5151e88bc2f4e4e6f5e914b5cb3cc8
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 21:23:36 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
+    a = []
+    if my_list == a:
         return None
     else:
         print(all([isinstance(item, int) for item in my_list]))

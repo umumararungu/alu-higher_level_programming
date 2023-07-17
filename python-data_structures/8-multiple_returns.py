5c6e01881bbda470e31d7da91e679a3077c0af45
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 20:59:41 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    if length > 0:
-        return length, first
-    else:
+    if len(sentence) == 0:
         return 0, None
+    else:
+        return len(sentence), sentence[0]

5622b84af890da3a64e4668895d5c18e1685d7cc
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 22:46:22 +0200

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,1 +1,8 @@
-11-delete_at.py
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+   if idx < 0 or idx > my_list:
+      return my_list
+   else:
+       space = ""
+       my_list[idx] = space
+       return my_list

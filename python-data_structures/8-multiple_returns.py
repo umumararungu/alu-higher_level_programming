d044225811dab3ce99780c368515f458a434889b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-17 20:12:00 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,5 @@
-8-multiple_returns.py
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sentence = "you are mine"
+    length, first = multiple_returns(sentence)
+    print("length: {:d} - first character: {}".format(length, first)

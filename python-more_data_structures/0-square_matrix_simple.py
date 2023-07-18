6db10737cf5384003832466134e7dbbf847caae1
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 13:21:51 +0200

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     if matrix != 0:
         for row in matrix:
             for col in row:
-                square = (matrix[col] ** 2)
+                square = (matrix[col]pow(2))
                 return square

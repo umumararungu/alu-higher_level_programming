b9e60167c296a5a3f6229f8d4a8ad4974d2cba76
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 15:03:41 +0200

    done

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return sorted(a_dictionary.values())
+    value = sorted(a_dictionary.keys())
+    for key in value:
+        print("{}: {}".format(key, a_dictionary[key]))

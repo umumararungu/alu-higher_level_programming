c2a0414962a0816970076194bcd6ccfdd523f2b7
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 14:02:01 +0200

    done

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
-        if my_list[i] == search:
+        if i == search:
             new_list.append(replace)
         else:
             new_list.append(i)

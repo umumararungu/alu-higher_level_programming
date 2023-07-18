22df557b33f89584c521b0953d5d93400f124da9
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 14:28:48 +0200

    done

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,9 +2,9 @@
 def common_elements(set_1, set_2):
     new_set = []
     for item_1 in set_1:
+        new_value = item_1
         for item_2 in set_2:
-            if set_1[item_1] == set_2[item_2]:
-                new_item = item_1
-                new_set.add(new_item)
+            if set_1[new_value] == set_2[item_2]:
+                new_set.add(new_value)
 
     return new_set

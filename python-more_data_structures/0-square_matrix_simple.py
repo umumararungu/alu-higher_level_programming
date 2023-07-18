184158c1495b18393244bc37c53ee48ab05ffaa6
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 13:14:39 +0200

    Update 0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix != 0:
+        for i in matrix:
+            square = (matrix[[i]] ** 2)
+            return suqare
+        else:
+            return None

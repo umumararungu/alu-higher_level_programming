b11c107f6cc2a40c1ccb818b31c13ccd50854e92
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 20:29:36 +0200

    done

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return (a_dictionary.pop(key))
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary

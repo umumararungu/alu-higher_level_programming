cee594ca3920625b5c0beabced4dcc2b16933cc1
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 20:48:31 +0200

    done

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    a_dictionary.update((key, value * 2) for key, value in a_dictionary.items()):
-    return a_dictionary
+    a_dictionary.update((key, value * 2) for key, value in a_dictionary.items()):    return a_dictionary

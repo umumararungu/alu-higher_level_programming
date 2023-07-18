593e31d393f34812b9fa93d1157f13bc8168d71e
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-18 14:13:55 +0200

    done

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    list_set = set(my_list)
-    uni = (list(list_set))
-    for i in uni:
-        return i
+    uni = list(dist.formkeys(my_list))
+    print(uni)

8ef7aa27b55a8b63c3dde5051f815e2e9eaf084f
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 14:12:58 +0200

    gg

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 class Square:
-"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass

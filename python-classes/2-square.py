e768a1360e949d4f58a2e0819c428971af307879
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 14:48:43 +0200

    done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def __init__(self, size=0):
+        self._Square__size = size
+        size_type =type(size)
+        if size_type != 'int':
+            print("size must be an integer")
+        elif size < 0:
+            print("size must be >= 0")

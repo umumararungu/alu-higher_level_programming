11c4aa4e71a3d70d003a441255da3ecd6f4a7bf5
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 16:49:45 +0200

    done

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,8 +6,9 @@ class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -20,6 +21,14 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, int):
+            raise TypeError("postion must be a turple of 2 positive integers")
 
     def area(self):
         return self.__size ** 2

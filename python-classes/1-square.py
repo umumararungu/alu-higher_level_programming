686537c9224442ac466c6e360ce86d66dc567105
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 14:29:11 +0200

    done

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
-"""python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-"""python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
-class Square(size):
+
+
+class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass

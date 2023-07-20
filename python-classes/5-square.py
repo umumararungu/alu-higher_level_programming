df5ab59b66d7eb04582ad52dad17900e79fc046c
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 16:27:10 +0200

    done

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print(\n)
+        else:
+            for i in range(self.__size):
+                print("#", end="")

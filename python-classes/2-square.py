883bc23f41470bb1f093962cce889521283d418a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 14:50:52 +0200

    done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,8 +8,5 @@ class Square:
 
     def __init__(self, size=0):
         self._Square__size = size
-        size_type =type(size)
-        if size_type != 'int':
-            print("size must be an integer")
-        elif size < 0:
+        if size < 0:
             print("size must be >= 0")

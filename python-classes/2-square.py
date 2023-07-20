1a3ddd00b350fc47f86782cba155577872c60ea7
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 14:56:48 +0200

    done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,4 +9,8 @@ class Square:
     def __init__(self, size=0):
         self._Square__size = size
         if size < 0:
-            print(e)
+            try:
+                print(size)
+            except ValueError as e:
+                print(f"size must be >= 0")
+            

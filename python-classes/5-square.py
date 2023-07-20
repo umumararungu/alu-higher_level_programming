f71d91dcaeb45f19a89c4a5f7f7db4d41da6b771
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 16:52:34 +0200

    done

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -36,6 +36,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+        elif position[1] > 0:
+            print("")
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            print(" ")

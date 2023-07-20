036df882b6dbe72db59036f7bfbb814b58b2f8d9
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 15:52:26 +0200

    done

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def __init__(self, size=0):
+        self._Square__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        return self.__size
+       
+    def area(self):
+        return self.__size ** 2

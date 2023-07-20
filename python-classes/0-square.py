abd06d3e99affda227783fdb6dce5b2aa31f0d4e
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 13:47:20 +0200

    Sqaure

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    pass
-    my_square = Square()
-    print(type(mysquare))
-    print(mysquare.__dict__)
+    
+my_square = Square()
+print(type(mysquare))
+print(mysquare.__dict__)

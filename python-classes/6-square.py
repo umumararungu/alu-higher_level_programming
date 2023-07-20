d8a0bfcc661d64260cc06525da4a6a740e58e8ca
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 17:13:57 +0200

    done

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -28,7 +28,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple, int) or len(value) != 2 ):
+        if not isinstance(value, tuple, int) or len(value) != 2:
             raise TypeError("position must be a turple of 2 positive integers")
         self.__position = value
 

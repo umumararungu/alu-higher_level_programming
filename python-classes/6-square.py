8eb63c7a940998b663c64afe9c4eb08514b5e422
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 17:24:53 +0200

    done

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,8 +5,6 @@
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass
-
-
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position

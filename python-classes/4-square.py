aad16611ed71dc7b768ebf80596cac443c1adeb8
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-20 16:10:30 +0200

    done

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,6 +17,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2

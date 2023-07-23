74b8f7aabd3e18620e7f07846c179d5cb29e20db
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-23 21:17:20 +0200

    8-rectabgle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_intances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):

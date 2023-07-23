5715eab59e12a38318e6020b293958b4322ea69f
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-23 18:32:56 +0200

    1-rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -2,6 +2,36 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
-class Square:
+class Rectangle:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass
+
+
+    def __init__(self, width=0, heiht=0):
+        self.__width = width
+        self.__height = height
+
+
+    @property
+    def width(self):
+        self.__width = width
+        return width
+
+    @property.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >=0")
+
+    @property
+    def height(self):
+        self.__height = height
+        return height
+
+    @property.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >=0")

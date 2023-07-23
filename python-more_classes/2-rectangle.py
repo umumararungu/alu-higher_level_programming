e2549f657aa1da11be3ba50a606fd59f70573819
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-23 18:59:03 +0200

    2-rectangle

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0

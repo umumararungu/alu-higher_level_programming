ca433e053f3171b5e63a6b0331ba450679b1f67b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-23 21:21:14 +0200

    8-rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("react_1 must be an instance of Rectangle")
-        elif not isinstance(rect_, Rectangle):
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         else:
             if rect_1 == rect_2 or rect_1 > rect_2:

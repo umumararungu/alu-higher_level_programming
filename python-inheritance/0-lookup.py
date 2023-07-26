cf9090c4cef6c8be197b334e5b95aa70b427beeb
root <umcynthi@gmail.com>
2023-07-26 03:47:20 -0700

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").__doc__)'''
 def lookup(obj):
+    ''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
     return dir(obj)

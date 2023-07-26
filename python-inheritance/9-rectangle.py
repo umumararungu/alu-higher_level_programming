8887390f58c5fdd0031fe49712ea2893a173d341
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 14:44:47 +0200

    area

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
 
     def area():
-        return self.width * self.height
+        return self.__width * self.__height
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"

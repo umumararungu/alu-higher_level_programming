040116ff282163528cc889b2b6445a6d60d5011f
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 13:38:42 +0200

    geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass

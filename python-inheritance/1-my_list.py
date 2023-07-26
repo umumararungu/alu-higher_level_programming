41640413835f08d194d2bdfabc9830e5954b42bc
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 12:57:57 +0200

    mylist

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").__doc__)'''
+
+
+class Mylist(list):
+    '''python3 -c 'print(__import__("my_module").MyClass.__doc__)''''
+    pass
+ 
+    def append(self, value):
+        super().append(value)
+        self.sort()
+
+    def print_sorted(self):
+        print(sorted(self))

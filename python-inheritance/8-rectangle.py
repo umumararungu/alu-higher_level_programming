a1a06f6cf7fed7d036ebee98a8c59f3903b9ff63
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 14:31:57 +0200

    done

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -18,14 +18,12 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    pass
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.height}"

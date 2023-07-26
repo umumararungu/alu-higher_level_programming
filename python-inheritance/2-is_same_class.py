38b1d71c6be652fffedafdc1f3f629dc640e1a16
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 13:21:57 +0200

    same

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,5 +6,5 @@ def is_same_class(obj, a_class):
     '''+python3 -c 'print(__import__("my_module").my_function.__doc__)'''
     if isinstance(obj, a_class):
         return True
-    else:
+    if not isinstance(obj, a_class):
         return False

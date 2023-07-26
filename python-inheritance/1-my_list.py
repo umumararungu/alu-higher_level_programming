c0ea2821862adb067c8ec2c0541dd94c7ac68705
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 13:12:06 +0200

    myl

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,8 +7,10 @@ class Mylist(list):
     pass
  
     def append(self, value):
+        """+python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
         super().append(value)
         self.sort()
 
     def print_sorted(self):
+        """+python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
         print(sorted(self))

f57abd11c03be9576a9ea4a6c48dd672f3f207a6
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 13:41:11 +0200

    area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    

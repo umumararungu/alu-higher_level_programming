79af60633f1e1636f27e2567fe8530dc443a45d5
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 14:54:51 +0200

    done

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
-Square = __import__('10-square').Square
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""

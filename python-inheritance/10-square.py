4badeb6f9bff7736ea3f1b2747ed2c3015ee9c8b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 15:04:06 +0200

    done

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").__doc__)'"""
+# class 'Square' that inherits from 'Rectangle'
+# (9-rectangle.py)
+"""
+    define a class 'Square' inheriting from 'BaseGeometry'
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    pass
+    """
+        representation of 'Square
+    """
 
     def __init__(self, size):
+        """
+            instantiate private instance field size
+        """
+
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.integer_validator("size", size)
+        self.__size = size

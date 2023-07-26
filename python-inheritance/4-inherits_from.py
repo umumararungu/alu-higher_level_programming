978fb48eb5a0de78692c641aa8f311efdd4f5199
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 13:37:06 +0200

    class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''+python3 -c 'print(__import__("my_module").my_function.__doc__)'''
-    return type(obj) is type(a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

12a67e95593cde8d26891826104d877601d3f424
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 14:19:01 +0200

    subclass

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def __init__(self, width, height):
+        super().integer_validator(self, value="width")
+        super().integer_validator(self, value="height")
+        self.width = width
+        self.height = height

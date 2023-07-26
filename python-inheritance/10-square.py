8d31559d9bbe808049721e1d5dd53b66462ab53b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 14:52:58 +0200

    done

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    pass
-
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Square = __import__('10-square').Square
 
 class Square(Rectangle):
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""

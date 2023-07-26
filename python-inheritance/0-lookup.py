4fb8232b042e3f22cb3bb340db8997e96f40d587
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-26 12:50:00 +0200

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''python3 -c 'print(__import__("my_module").__doc__)'''
+
+
 def lookup(obj):
     '''+python3 -c 'print(__import__("my_module").my_function.__doc__)'''
     return dir(obj)

45b12dd9ea499e6f24f0a917b70d92a976b64556
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-27 23:33:58 +0200

    save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").__doc__)'"""
+"""
+7-add_item.py
+
+Script that adds all arguments to a Python list and saves them to a JSON file.
+"""
+
 import sys
 import os.path
-from os import path
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 def add_items_to_list(filename, *args):
-    my_list = []
-    if path.exists(filename):
+    """
+    Add all arguments to a Python list and save them to a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to save the list.
+        *args (list): Variable-length argument list containing items to add to the list.
+
+    Returns:
+        list: The updated list containing all the items.
+    """
+    if os.path.isfile(filename):
         my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
     my_list.extend(args)
-    save_to_json_file(my_list, filename)
+    save_to_json_file(filename, my_list)
+
+    return my_list
 
 if __name__ == "__main__":
     filename = "add_item.json"
     args = sys.argv[1:]
-    add_items_to_list(filename, *args)
+    updated_list = add_items_to_list(filename, *args)

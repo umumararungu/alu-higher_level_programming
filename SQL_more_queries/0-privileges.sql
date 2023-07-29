8cf5b27dc5cdd120406bb1f1e0c1b65cc8889f69
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-29 14:01:25 +0200

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- check privileges
-SHOW GRANT FOR 'user_0d_1','user_0d_2';
+CREATE USER 'user_0d_1'@'localhost','user_0d_2'@'localhost';
+GRANT ALL privileges ON *.* TO 'user_0d_1'@'localhost','user_0d_2'@'localhost';

68166b73c8de3a5bcb0ca370364ad5c073606d8c
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-29 12:06:47 +0200

    second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,3 @@
 --create second table
-CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256),score INT) values(1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);
+CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256),score INT);
+INSERT INTO second_table values(1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);

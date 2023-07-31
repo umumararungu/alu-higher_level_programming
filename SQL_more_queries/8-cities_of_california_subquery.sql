06fa107a006160825b97bcdcac981cf4da25ad53
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-31 18:57:34 +0200

    done

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- california cities
-SELECT id,name FROM cities WHERE states_id=( SELECT id FROM states WHERE name='Calfornia') ORDER BY id;
+SELECT id,name FROM cities WHERE states_id=( SELECT id FROM states WHERE name='Calfornia') and id=state_id ORDER BY id;

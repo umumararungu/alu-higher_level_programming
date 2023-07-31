5c41388b4075bd2a626a1a6e03817dae3c46c6af
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-31 19:34:21 +0200

    done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,4 @@
 -- tvshow
-CREATE DATABASE hbtn_0d_tvshows;
-mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id

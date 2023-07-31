ae65f01dd6d58b9bb90cd477e34450d2380f30f1
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-31 19:02:18 +0200

    dooooo

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- california cities
-SELECT states.id,cities.id,cities.name FROM cities,states WHERE states.id=( SELECT id FROM states WHERE name='Calfornia') and cities.id=states.id ORDER BY cities.id;
+SELECT states.id,cities.id,cities.name FROM cities,states WHERE states.id=( SELECT id FROM states WHERE name='Calfornia') and cities.id=states.id ORDER BY cities.id ASC;

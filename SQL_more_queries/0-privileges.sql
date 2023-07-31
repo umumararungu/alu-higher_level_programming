f55c67bdc1af7406fa0f3b235573d58b030ab6ed
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-31 20:10:39 +0200

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
 -- check privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH auntentication_plugin BY 'password1';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH auntentication_plugin BY 'password2';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # body size
-response_size=$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}')
-echo "$response_size"
+response=$(curl -sI "$1")

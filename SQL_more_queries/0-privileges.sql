4341f3859df04d7f40a030a2d86702d67b2641c9
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-07-31 17:53:34 +0200

    DONE

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
 -- check privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH auntentication_plugin BY 'password1';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH auntentication_plugin BY 'password2';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;

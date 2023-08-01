8a71afe68a94011166b5a2c40189c48c90ccaf14
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-01 14:43:57 +0200

    save

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- create a user on server and assign all privileges
--- create user
+-- Create user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
--- assign all privileges to everything
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+FLUSH PRIVILEGES;

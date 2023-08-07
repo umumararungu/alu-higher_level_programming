238df8870db7c0b756816a077a38c6751dcdd8d2
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 15:37:25 +0200

    square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,6 +2,15 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
-def say_my_name(first_name, last_name=""):
+def print_square(size):
     """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-    
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >=0")
+
+    if(isinstance(size, float) and size < 0):
+        raise TypeError("size must be an integer")
+
+    print(size * "#")

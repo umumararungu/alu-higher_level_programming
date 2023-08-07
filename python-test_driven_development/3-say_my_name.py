38f3923b6d262fc5878e90ac2bb62a8e59e1e487
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 15:22:29 +0200

    my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+def say_my_name(first_name, lastname=""):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    if not isinstance(first_name, string):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(lat_name, string):
+        raise TypeError("last_name must be a string")
+    
+    print("My name is <first_name> <last_name>")

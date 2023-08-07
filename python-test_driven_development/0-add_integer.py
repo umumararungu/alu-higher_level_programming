009753814e2eec52f0e9600c5e417534a622e7b2
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 13:55:39 +0200

    saved

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,11 @@
 def add_integer(a, b=98):
     """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
     if not isinstance(a, int):
-        raise TypeError("a must be an integere")
+        raise TypeError("a must be an integer")
     if not isinstance(a, float):
-        raise TypeError("a must be an integere")
+        raise TypeError("a must be an integer")
     if not isinstance(b, int):
-        raise TypeError("a must be an integere")
+        raise TypeError("a must be an integer")
     if not isinstance(b, float):
-        raise TypeError("a must be an integere")
+        raise TypeError("a must be an integer")
     return int(a)+int(b)

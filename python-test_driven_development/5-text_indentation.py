6e2de43a53152b841cff2ba4a2bdeb18c3d0e85f
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 15:58:58 +0200

    text

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+def text_indentation(text):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    suffix = [".", "?", ":"]
+
+    result = text.endwith(suffix)
+
+    if result == True:
+        print(text, "\n", "\n")
+
+    else:
+        print("")

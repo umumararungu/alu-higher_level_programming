ebf869a4e61628d1fc9d10c35ab11e14256e7fe2
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 14:49:48 +0200

    add integer

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+def matrix_divided(matrix, div):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    if(not isinstance)
+
+ 

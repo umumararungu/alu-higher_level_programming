eee4f60a5c9b9c3f01c0b19472aab3d1dca754da
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 15:25:21 +0200

    my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,7 @@ def say_my_name(first_name, lastname=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if not isinstance(lat_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     
     print("My name is <first_name> <last_name>")

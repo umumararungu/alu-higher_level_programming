47ff4c06bfb7017077aaa14699476e584151ffdd
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-07 15:07:31 +0200

    matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,19 @@
 
 def matrix_divided(matrix, div):
     """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-    if(not isinstance)
+    if(not isinstance(matrix, int) and not isinstance(matrix, float)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
+    if len(set(map(matrix))) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if(not isinstance(div, int) and not isinstance(div, float)):
+        raise TypeError("div must be an number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = round((matrix / div), 2)
+
+    return new_matrix
  

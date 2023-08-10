7db2d1f4bef51bedf68300f0508bf1e6a3127932
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-10 16:32:15 +0200

    kwrags

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,18 +108,21 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.__width, self.__height
         )
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        if len(args) > 0:
+        if len(args) >= 1:
             self.id = args[0]
-        if len(args) > 1:
+        if len(args) >= 2:
             self.width = args[1]
-        if len(args) > 2:
+        if len(args) >= 3:
             self.height = args[2]
-        if len(args) > 3:
+        if len(args) >= 4:
             self.x = args[3]
-        if len(args) > 4:
+        if len(args) >= 5:
             self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
 
 if __name__ == "__main__":

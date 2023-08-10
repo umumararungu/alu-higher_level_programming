e641814ec3f287c3f37e4a5bbd7cb64b7e9ede07
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-10 16:26:23 +0200

    args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -110,7 +110,16 @@ class Rectangle(Base):
     
     def update(self, *args):
         """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        print(args, type(args))
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
 
 
 if __name__ == "__main__":

3a392491193bd8acfc3c487d3188d09d9dfee48a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-10 14:40:19 +0200

    rec

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,82 +2,57 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
-class Base:
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 class Rectangle(Base):
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    #getter method
-    def get_width(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        return self.get__width
-    #setter method
-    def set_width(self, width):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        self.__width = width
 
-    #getter method
-    def get_height(self):
+        def __init__(self, width, height, x=0, y=0, id=None):
         """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        return self.get__height
-    #setter method
-    def set_height(self, height):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        self.__height = height
 
-    #getter method
-    def get_x(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        return self.get__x
-    #setter method
-    def set_x(self, x):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        self.__x = x
-
-    #getter method
-    def get_y(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        return self.get__y
-    #setter method
-    def set_y(self, y):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        self.__y = y
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
+    # getter method
+    def get_width(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return self.get__width
+    # setter method
+    def set_width(self, width):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        self.__width = width
+
+    # getter method
+    def get_height(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return self.get__height
+    # setter method
+    def set_height(self, height):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        self.__height = height
+
+    # getter method
+    def get_x(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return self.get__x
+    # setter method
+    def set_x(self, x):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        self.__x = x
+
+    # getter method
+    def get_y(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        return self.get__y
+    # setter method
+    def set_y(self, y):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        self.__y = y
 
 if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
 
     r1 = Rectangle(10, 2)
     print(r1.id)

2649b6938664d5de7a6140b89b762a369eed732a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-16 14:37:17 +0200

    8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ prints the first State object from the
 database hbtn_0e_6_usa"""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"

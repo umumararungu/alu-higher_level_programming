91be7f0ec4c3f42f9d802be10d938ee8e4926416
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-16 11:40:40 +0200

    11 square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,12 +11,34 @@ class Square(Rectangle):
         """ Initializes a Square instance """
         super().__init__(size, size, x, y, id)
 
+    
+@property
+    def size(self):
+        """ Getter method for size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for size """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ Returns a string representation of the Square """
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
 if __name__ == "__main__":
+    s1 = Square(5)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
     s1 = Square(5)
     print(s1)
     print(s1.area())

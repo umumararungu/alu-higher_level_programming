df25416d6f54aedc6eeb57668981dec85011588c
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-08-16 11:43:35 +0200

    12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,7 +3,6 @@
 
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
     """ Represents a Square, a special case of Rectangle """
 
@@ -22,19 +21,41 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """ Updates attributes of the Square """
+        if args and len(args) > 0:
+            attrs = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """ Returns a string representation of the Square """
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
 if __name__ == "__main__":
     s1 = Square(5)
     print(s1)
-    print(s1.size)
-    s1.size = 10
+
+    s1.update(10)
     print(s1)
 
-    try:
-        s1.size = "9"
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    s1.update(1, 2)
+    print(s1)
+
+    s1.update(1, 2, 3)
+    print(s1)
+
+    s1.update(1, 2, 3, 4)
+    print(s1)
+
+    s1.update(x=12)
+    print(s1)
+
+    s1.update(size=7, y=1)
+    print(s1)
+
+    s1.update(size=7, id=89, y=1)
+    print(s1)

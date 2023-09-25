ddf6936238e689bf40332c30ccf9b77539bc3b4b
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-09-25 12:03:58 +0200

    done

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,4 @@ const secondLine = 'Python is cool';
 const thirdLine = 'JavaScript is amazing';
 console.log(firstLine);
 console.log(secondLine);
-console.log(thirdLineLine);
+console.log(thirdLine);

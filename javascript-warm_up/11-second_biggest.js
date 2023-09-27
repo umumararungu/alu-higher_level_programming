5be2d983aa9431dd1f7111ff50c4ca3ffd6670f6
umumararungu <umcynthi@gmail.com>
2023-09-27 21:29:27 +0200

    second

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function secondLargest (arr) {
+  if (arr.length <= 1) {
+    return 0;
+    }
+  arr = arr.map(Number).sort((a, b) => b - a);
+  return arr[1];
+}
+const args = process.argv.slice(2);
+console.log(secondLargest(args));

36c32e425961fab9a68acb3bb0ba34ee0450c999
umumararungu <umcynthi@gmail.com>
2023-09-27 16:09:49 +0200

    c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const argument = parseInt(process.argv[2]);
 
-for (let i = 0; i <= argument; i++) {
-  console.log('C is fun');
+if (isNaN(argument)) {
+    for (let i = 0; i <= argument; i++) {
+        console.log('C is fun');
+      }
+      
+} else{
+  console.log('Missing number of occurrences');
 }

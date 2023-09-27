486ac8cd9b159385efc6301cd79f0f82eab7ecf2
umumararungu <umcynthi@gmail.com>
2023-09-27 20:58:35 +0200

    fact

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n) || n < 0) {
-      return 1;
-    } else if (n === 0) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  
-  const input = parseInt(process.argv[2]);
-  const result = factorial(input);
-  console.log(result);
+}
+
+const input = parseInt(process.argv[2]);
+const result = factorial(input);
+console.log(result);
   

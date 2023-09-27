c681d8ebdbc7df4619fffc8e0ec5410054b8f4f7
umumararungu <umcynthi@gmail.com>
2023-09-27 16:49:01 +0200

    function

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function add(a, b) { return a + b; }
+function add (a, b) { return a + b; }
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
-let result = add(arg1, arg2);
+const result = add(arg1, arg2);
 
 console.log(result);

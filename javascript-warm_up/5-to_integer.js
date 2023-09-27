ce73fff69cb2f4998387ee746866dfe578262ed3
umumararungu <umcynthi@gmail.com>
2023-09-27 15:47:33 +0200

    num

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const arg1 = parseInt(process.argv[2]);
 if (!isNaN(arg1)) {
-    console.log('My number: ' + arg1);
+  console.log('My number: ' + arg1);
 }
-else{
-    console.log('Not a number');
+else {
+  console.log('Not a number');
 }

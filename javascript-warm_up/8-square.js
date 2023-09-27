24d351cf33e500d1665ad22e8f2372fc5bd1348e
umumararungu <umcynthi@gmail.com>
2023-09-27 16:24:30 +0200

    x

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const argument = parseInt(process.argv[2]);
+
+if (isNaN(argument)) {
+  console.log('Missing size');
+} else {
+  for (let row = 1; row <= argument; row++) {
+    for (let col = 1; col >= row; col++)
+    console.log('x');
+  }
+  console.log('x');
+}
+}

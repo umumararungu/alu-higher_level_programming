836b7d3b1f64c7037835ceb70518a7347108fa04
umumararungu <umcynthi@gmail.com>
2023-09-27 22:05:24 +0200

    fun

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
-callMeMoby(x, function () {
-  console.log('C is fun');
-});
+
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = {
+  callMeMoby: callMeMoby
+};

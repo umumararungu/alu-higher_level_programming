645a5a2f4b26e2536f13140995d035360e02723e
umumararungu <umcynthi@gmail.com>
2023-09-27 15:19:25 +0200

    val

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (process.argv !==0){
-    console.log(Process.argv);
-}
-else{
-    console.log('No argument')
+
+const argument = process.argv[2];
+
+if (argument === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argument);
 }

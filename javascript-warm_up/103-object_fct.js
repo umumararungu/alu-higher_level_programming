758c699f6990347b0745201cb7012218354de7f5
umumararungu <umcynthi@gmail.com>
2023-09-30 14:23:17 +0200

    ob

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+number === myObject.value
+  function incr (number, theFunction) {
+    number += 1;
+    theFunction(number);
+  }
+console.log(incr)

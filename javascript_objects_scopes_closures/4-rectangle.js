df1ae5de3a8e6d2737785d9dcd5944dfe4fae00b
umumararungu <umcynthi@gmail.com>
2023-10-01 21:23:37 +0200

    done

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-        return;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
       }
-      this.width = w;
-      this.height = h;
+      console.log(line);
     }
-    print() {
-      for (let i = 0; i < this.height; i++) {
-        let line = '';
-        for (let j = 0; j < this.width; j++) {
-          line += 'X';
-        }
-        console.log(line);
-      }
-    }
-    double() {
-      this.height *= 2;
-      this.width *= 2;
-    }
+  }
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
   
-    rotate() {
-      let exchange;
-      exchange = this.width;
-      this.width = this.height;
-      this.height = exchange;
+  rotate () {
+    let exchange;
+    exchange = this.width;
+    this.width = this.height;
+    this.height = exchange;
   }
 }
   

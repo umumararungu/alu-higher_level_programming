d0f32dee3c47d7f5324aedc0f8e4a8f212349feb
umumararungu <umcynthi@gmail.com>
2023-10-01 15:10:24 +0200

    done

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    const length = list.length;
+    const newList = [];
+    for (let i = length - 1; i >= 0; i--) {
+      newList.push(list[i]);
+    }
+    return newList;
+  };

997e70e4ac1e9cbc17a3b963f09fe821498fb5af
umumararungu <umcynthi@gmail.com>
2023-10-01 21:39:03 +0200

    correct

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -3,18 +3,7 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function() {
+  incr: function () {
     this.value += 1;
   }
 };
-
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);

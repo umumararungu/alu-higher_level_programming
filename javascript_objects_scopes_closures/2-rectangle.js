4100909fbe72e6d7b90990a505cfc7e9c3840def
umumararungu <umcynthi@gmail.com>
2023-10-01 21:17:49 +0200

    done

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return {};
-    
     }
   }
-
 }
-    
 module.exports = Rectangle;
-  

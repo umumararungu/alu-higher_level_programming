3717a970e6b522be4aa49895a844d6b8b3d32b0a
umumararungu <umcynthi@gmail.com>
2023-10-03 10:02:38 +0200

    read

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs= require ('fs');
-const path = process.argv[2];
+const fs = require ('fs');
+const filePath = process.argv[2];
 
-fs.readFile(path, 'utf-8', (error, data) => {
+fs.readFile(filePath, 'utf-8', (error, data) => {
     if (error){
         console.error(error);
     }

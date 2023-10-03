43eff5b6a6ae4c66883be36348a896f717c571f2
umumararungu <umcynthi@gmail.com>
2023-10-03 09:42:10 +0200

    done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const file= require ('file') 
-file.readFile('README.md', (err, inputD) => {
-    if (err) throw err;
-    console.log(inputD.toString());
-})
+const file= require ('file');
+const path = process.argv[2];
+
+file.readFile(path, 'utf-8', (error, data) => {
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(data);
+    }
+
+});

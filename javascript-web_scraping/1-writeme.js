91a464948f29f28529d9472707c08eece0fd20cb
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-10-05 12:48:11 +0200

    again

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 
-const req = require('request');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-req.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-  }
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });

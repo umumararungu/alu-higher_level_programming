8605bb62237360c8148b0151ad9670445c1f30b1
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-10-06 13:02:39 +0200

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
-#!/home/codespace/nvm/current/bin/node
+#!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], (err, contents) => {
   err ? console.log(err) : console.log(contents.toString());

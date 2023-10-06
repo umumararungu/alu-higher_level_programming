02b5a9d229f5d977636f7ff5cb1f93914708232a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2023-10-06 10:50:01 +0000

    path

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/home/codespace/nvm/current/bin/node
 const fs = require('fs');
 const path = process.argv[2];
-fs.readFile('path', 'utf8', function (error, content) {
+fs.readFile('path', 'utf-8', function (error, content) {
   console.log(error || content);
 });
